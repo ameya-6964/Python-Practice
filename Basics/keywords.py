8bccfbd468ca3b5e9e985ade3837768955999b12
ameya-6964 <belvalkar7@gmail.com>
2022-12-25 21:43:21 +0530

    Added Keywords File

---
+++ Basics/keywords.py
@@ -0,0 +1,23 @@
+#False, await, else, import, pass, None, break, except, in,
+#raise, True, class, finally, is, return, and, continue, for, lambda, try, as,
+#def, from, nonlocal, while, assert, del, global,
+#not, with, async, elif, if, or, yield
+# These Are Common Keywords In Python And Cannot Be used As Variable Names
+
+a=10
+b=12.3
+c='Ameya'
+d='Belvalkar'
+
+print(a,b,c,d)
+
+# OUTPUT : - 10 12.3 Ameya Belvalkar
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+
+
+# Here a b c d are Variables and 10,12.3,Ameya,Belvalakr Are Values Or Can Be Called As Literals 
