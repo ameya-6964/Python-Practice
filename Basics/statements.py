46860443b41d5d9b52f2b0cd54a5dc75b90911a3
ameya-6964 <belvalkar7@gmail.com>
2022-12-25 21:52:12 +0530

    Added Statements

---
+++ Basics/statements.py
@@ -0,0 +1,27 @@
+# Multi-line statement
+# In Python, the end of a statement is marked by a newline character. But we can make a statement extend over
+# multiple lines with the line continuation character (\). For example:
+a = 1 + 2 + 3 + \
+4 + 5 + 6 + \
+7 + 8 + 9
+
+print(a)
+
+#OUTPUT : - 45
+
+# Can Also Be Written As Using () instead Of / 
+
+b = (1 + 2 + 3 +
+4 + 5 + 6 +
+7 + 8 + 9)
+
+print(b)
+
+#OUTPUT : - 45
+
+# Can Also Be Written As Using [] 
+colors = ['red','blue','green']
+
+print(colors)
+
+# OUTPUT : -  ['red', 'blue', 'green']
